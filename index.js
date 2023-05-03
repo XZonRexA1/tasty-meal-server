19479c4e7e82876069d9ef312a9c94b6ed84a2f6
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-05-03 23:35:12 +0600

    specific data load method has been appled to chefDetials and chefRecipes

--- index.js
+++ index.js
@@ -19,6 +19,24 @@ app.get("/chefRecipes", (req, res) => {
   res.send(chefRecipes);
 });
 
+app.get("/chefRecipes/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedChefRecipes = chefRecipes.find((r) => r._id === id);
+  res.send(selectedChefRecipes);
+});
+
+app.get("/chefDetails/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id === 0) {
+    res.send(chefRecipes);
+  } else {
+    const chefDetailsRecipes = chefRecipes.filter(
+      (r) => parseInt(r.chefDetail_id) === id
+    );
+    res.send(chefDetailsRecipes);
+  }
+});
+
 app.listen(port, () => {
   console.log(`Tasty Meal API is running on port: ${port}`);
 });
