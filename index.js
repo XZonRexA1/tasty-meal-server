b2ad7d57472507c9e81d818f88a726005b10a248
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-05-04 14:34:42 +0600

    only chef details recieps json file added

--- index.js
+++ index.js
@@ -3,39 +3,20 @@ const app = express();
 const cors = require("cors");
 const port = 5000;
 
-const chefDetails = require("./data/chefDetails.json");
-const chefRecipes = require("./data/chefRecipes.json");
+const chefDetailsRecipes = require("./data/chefDetailsRecipes.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Tasty Meal is running");
 });
 
-app.get("/chefDetails", (req, res) => {
-  res.send(chefDetails);
+app.get("/chefDetailsRecipes", (req, res) => {
+  res.send(chefDetailsRecipes);
 });
 
-app.get("/chefRecipes", (req, res) => {
-  res.send(chefRecipes);
-});
 
-app.get("/chefRecipes/:id", (req, res) => {
-  const id = req.params.id;
-  const selectedChefRecipes = chefRecipes.find((r) => r._id === id);
-  res.send(selectedChefRecipes);
-});
 
-app.get("/chefDetails/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  if (id === 0) {
-    res.send(chefRecipes);
-  } else {
-    const chefDetailsRecipes = chefRecipes.filter(
-      (r) => parseInt(r.chefDetail_id) === id
-    );
-    res.send(chefDetailsRecipes);
-  }
-});
 
 app.listen(port, () => {
   console.log(`Tasty Meal API is running on port: ${port}`);
