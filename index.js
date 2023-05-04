5ca3608fc21bc7aa3bdd06cdc418db3ab33865de
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-05-04 19:19:16 +0600

    proccess evniromental port added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const chefDetailsRecipes = require("./data/chefDetailsRecipes.json");
 
